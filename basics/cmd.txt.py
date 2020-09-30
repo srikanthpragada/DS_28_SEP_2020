faa729793cebf27933dddd68b1dee19815d4f7aa
srikanthpragada <srikanthpragada@gmail.com>
2020-09-30 20:59:54 +0530

    30-SEP

---
+++ basics/cmd.txt.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+print('Welcome To Jupyter Notebook')
+print('Welcome To Jupyter Notebook')
+from datetime import datetime
+datetime.now()
+a = 10 
+# Print squares of number from 1 to 10
+for i in range(1,11):
+    print(i*i)
+get_ipython().run_line_magic('pinfo', 'datetime.strptime')
+*This is italic*
+from IPython.display import Image
+Image(url ="http://www.srikanthtechnologies.com/banner.jpg")
+from IPython.display import Image
+Image(url ="http://www.srikanthtechnologies.com/banner.jpg")
